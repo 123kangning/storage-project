5f4c6d3a906c69b859db3364038b48ceca022b27
123kangning <3390142252@qq.com>
2023-08-02 10:47:11 +0800

    删除一些已废弃的内容

--- go-object-storage/src/lib/utils/utils.go
+++ go-object-storage/src/lib/utils/utils.go
@@ -4,39 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"io"
-	"net/http"
-	"strconv"
-	"strings"
 )
-
-func GetOffsetFromHeader(h http.Header) int64 {
-	byteRange := h.Get("range")
-	if len(byteRange) < 7 {
-		return 0
-	}
-	if byteRange[:6] != "bytes=" {
-		return 0
-	}
-	bytePos := strings.Split(byteRange[6:], "-")
-	offset, _ := strconv.ParseInt(bytePos[0], 0, 64)
-	return offset
-}
-
-func GetHashFromHeader(h http.Header) string {
-	digest := h.Get("digest")
-	if len(digest) < 9 {
-		return ""
-	}
-	if digest[:8] != "SHA-256=" {
-		return ""
-	}
-	return digest[8:]
-}
-
-func GetSizeFromHeader(h http.Header) int64 {
-	size, _ := strconv.ParseInt(h.Get("content-length"), 0, 64)
-	return size
-}
 
 // CalculateHash 计算该文件内容的hash值（文件内容从r中读取）
 func CalculateHash(r io.Reader) string {
